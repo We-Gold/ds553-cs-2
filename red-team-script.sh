15c52564eb0e2750439b3c09fbc551118198358a
julian3sp <julianrespinal24@gmail.com>
2025-09-29 14:32:05 -0400

    red teams script changes

--- red-team-script.sh
+++ red-team-script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Script to run red team every 30 seconds from 11:59 PM to 11:59:59 PM
+# Script to run red team every 30 seconds from 11:59 PM to 12:01 AM
 
 while true; do
     current_time=$(date +"%H:%M")
-    if [ "$current_time" == "23:59" ]; then
+    if [ "$current_time" == "23:59" ] || [ "$current_time" == "00:00" ]; then
         # Run your red team command here
         ./run-red-team.sh
 
         # Wait for 30 seconds before running again
         sleep 30
+    elif [ "$current_time" == "00:01" ]; then
+        # Stop running at 12:01 AM
+        break
     else
-        # Sleep until 11:59 PM
+        # Sleep until the next check
         sleep 10
     fi
 done
